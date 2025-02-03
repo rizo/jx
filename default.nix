e90a27a481a510fb6d0277354db8356ef693a582
Rizo <rizo@odis.io>
2025-02-03 12:28:29 +0000

    Update deps and generated codegen for js_of_ocaml

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 let
   onix = import (builtins.fetchGit {
     url = "https://github.com/rizo/onix.git";
-    rev = "41bf9e887fa8f1399ac328f1868d6d2ba27aab9f";
+    rev = "969b4582eb7248330cf07d602723a26d7b4f4b71";
   }) { verbosity = "info"; };
 
 in onix.env {
@@ -14,7 +14,7 @@ in onix.env {
   };
 
   deps = {
-    "ocaml-system" = "*";
+    "ocaml-base-compiler" = "5.2.1";
     "ocamlformat" = "*";
     "ocaml-lsp-server" = "*";
     "odoc" = "*";
